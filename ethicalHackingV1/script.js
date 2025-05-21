0e6a41f817e9077397ddf36dc9d92edff18ce12b
Charlotte <charlotte.moolenaar@hva.nl>
2025-05-21 10:57:13 +0200

    Add files via upload

---
+++ ethicalHackingV1/script.js
@@ -0,0 +1,28 @@
+// -- timer and hint showing
+
+// console.log("test");
+
+// let hint = document.querySelector("#hint")
+// let timer = document.querySelector("#timer")
+// let secondsPassed = 0
+
+// setInterval(countSeconds, 1000)
+
+// function showHint() {
+//     hint.textContent = "Na 5 seconde komt er een hint."
+//     setTimeout(hideHint, 5000)
+// }
+
+// function hideHint() {
+//     hint.textContent = "Hint: Probeer speciale karakters…"
+// }
+
+// function countSeconds() {
+//     timer.textContent = secondsPassed
+//     secondsPassed += 1
+// }
+
+// showHint()
+
+// countSeconds()
+
